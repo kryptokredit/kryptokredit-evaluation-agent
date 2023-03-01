4b7ee6f710fd696044a96f091d9c32128ef1507d
giorgio-villani <15069760+giorgio-villani@users.noreply.github.com>
2023-03-01 06:42:09 +0000

    added requirements in underwriter

--- evaluation_agent/underwriter.py
+++ evaluation_agent/underwriter.py
@@ -9,6 +9,18 @@ import os
 load_dotenv()
 
 
+def fetch_signal(signal_names: List[str], adapter_inputs: Dict[str, Any]) -> Dict[str, Any]:
+   """Fetch signals from the decentralized signal portfolio service.
+   
+   For more details about DSP service, see https://github.com/00labs/huma-signals/tree/main/huma_signals
+   """
+   request = {"signal_names": signal_names, "adapter_inputs": adapter_inputs}
+   response = requests.post(config.signals_endpoint, json=request)
+   if response.status_code != 200:
+       raise ValueError(f"Error fetching signals: {response.text}")
+   return {k: v for k, v in response.json().get("signals").items() if k in signal_names}
+
+
 def underwrite(huma_pool, **kwargs):
     """
     The interface function between an EA and Huma EA service
@@ -20,20 +32,6 @@ def underwrite(huma_pool, **kwargs):
     """
 
     borrower_wallet_address = kwargs["borrowerWalletAddress"]  # noqa
-
-    requirements = {
-        "min_credit_score": 500,
-        "min_invoice_payments": 1,
-        "max_derogatory_marks": 0
-    }
-    
-    # to be removed
-    result = {
-            "credit": int(10000*10**6),
-            "intervalInDays": 30,
-            "remainingPeriods": 12,
-            "aprInBps": 0
-        }
     
     # your code here
     
@@ -58,5 +56,27 @@ def underwrite(huma_pool, **kwargs):
     #     }
     # else:
     #     raise Exception("accountTooNew")
+
+    requirements = {
+        "min_credit_score": 600,
+        "max_derogatory_marks": 0,
+        "min_invoice_payments": 10
+    }
+
+    if fetch_credit_score(borrower_wallet_address) < requirements["min_credit_score"]:
+        raise Exception("spectralCreditTooLow")
+
+    if fetch_derogatory_marks(borrower_wallet_address) != requirements["max_derogatory_marks"]:
+        raise Exception("derogatoryMarks")
+
+    if fetch_invoice_payments(borrower_wallet_address) < requirements["min_invoice_payments"]:
+        raise Exception("derogatoryMarks")
+
+    result = {
+            "creditLimit": int(100*10**6),
+            "intervalInDays": 30,
+            "remainingPeriods": 12,
+            "aprInBps": 0
+        }
     
     return result  # noqa
